ae8ea2f2419ba9c52f7aaf88f074a9a312cb5c7a
IuryAlves <iuryalves20@gmail.com>
2017-12-07 22:36:57 -0200

    Iniciando projeto

---
+++ core/tests.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from django.test.client import Client
+from django.shortcuts import reverse
+
+class IndexTestCase(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.client = Client()
+
+    def test_index_should_return_status_code_200(self):
+        response = self.client.get(reverse('index'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed('index.html')
+
